1ca1af0bf57ea956917dcc90845220a77c18cd36
Mohak Singh <75531763+mohak3singh@users.noreply.github.com>
2023-09-18 09:22:20 +0000

    Recursion_02 updates

--- Level-1/Recursion_02.java
+++ Level-1/Recursion_02.java
@@ -18,9 +18,32 @@ public class Recursion_02 {
         }
         return ans;
     }
+
+    public static String[] nokiaKeys = {".;","abc","def","ghi","jkl","mno","pqrs","tu","vwx","yz"};
+
+    public static ArrayList<String> getAllNokiaKeysCombinations(String str){
+        if(str.length() == 0){
+            ArrayList<String> base = new ArrayList<String>();
+            base.add("");
+            return base;
+        }
+
+        char ch = str.charAt(0);
+        String code = nokiaKeys[ch - '0'];
+        ArrayList<String> ans = new ArrayList<String>();
+        ArrayList<String> recAns = getAllNokiaKeysCombinations(str.substring(1));
+        for(int i = 0; i < code.length(); i++){
+            for(String s:recAns){
+                ans.add(code.charAt(i)+s);
+            }
+        }
+        return ans;
+    }
     
     public static void main(String[] args) {
-        String str = "abc";
-        System.out.println(getAllSubstrings(str));
+        String str = "abckl";
+
+        // System.out.println(getAllSubstrings(str));
+        System.out.println(getAllNokiaKeysCombinations("456"));
     }
 }
