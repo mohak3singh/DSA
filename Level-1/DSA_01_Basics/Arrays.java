5d600704bafaa4cfd5635620a01dc489bf2707e0
Mohak Singh <75531763+mohak3singh@users.noreply.github.com>
2023-09-21 11:13:03 +0000

    add arrays

--- Level-1/DSA_01_Basics/Arrays.java
+++ Level-1/DSA_01_Basics/Arrays.java
@@ -168,6 +168,70 @@ public class Arrays {
         }
     }
 
+    public static int binarySearch(int[] arr, int data) {
+        int si = 0, ei = arr.length - 1;
+        while (si <= ei) {
+            int mid = (si + ei) / 2;
+            if (arr[mid] == data)
+                return mid;
+            else if (arr[mid] < data)
+                si = mid + 1;
+            else
+                ei = mid - 1;
+        }
+
+        return -1;
+    }
+
+    public static int firstIndex(int[] arr, int data) {
+        int si = 0, ei = arr.length - 1;
+        while (si <= ei) {
+            int mid = (si + ei) / 2;
+            if (arr[mid] == data) {
+                if (mid - 1 >= 0 && arr[mid - 1] == data)
+                    ei = mid - 1;
+                else
+                    return mid;
+            } else if (data < arr[mid])
+                ei = mid - 1;
+            else
+                si = mid + 1;
+        }
+
+        return -1;
+    }
+
+    public static int lastIndex(int[] arr, int data) {
+        int n = arr.length;
+        int si = 0, ei = n - 1;
+        while (si <= ei) {
+            int mid = (si + ei) / 2;
+            if (arr[mid] == data) {
+                if (mid + 1 < n && arr[mid + 1] == data)
+                    si = mid + 1;
+                else
+                    return mid;
+            } else if (data < arr[mid])
+                ei = mid - 1;
+            else
+                si = mid + 1;
+        }
+
+        return -1;
+    }
+
+    public static void FirstAndLastIdx(int[] arr, int data) {
+        int fi = firstIndex(arr, data);
+        if (fi == -1) {
+            System.out.println("Data Not Found!");
+            return;
+        }
+
+        int li = lastIndex(arr, data);
+
+        System.out.println(fi);
+        System.out.println(li);
+    }
 
     public static void main(String[] args) {
         int[] arr = {3,4,0,1,2};
