11bcb1b8fa9c8967141d3cb553d93f7dae9e5cce
Mohak Singh <75531763+mohak3singh@users.noreply.github.com>
2023-09-29 12:07:21 +0000

    Linkedlist updated

--- Level-1/DSA_06_LinkedList/Client.java
+++ Level-1/DSA_06_LinkedList/Client.java
@@ -4,5 +4,6 @@ public class Client {
     public static void main(String[] args) {
         LinkedList_01 ll = new LinkedList_01();
         ll.addFirst(10);
+        System.out.println(ll.removeLast());
     }
 }
--- Level-1/DSA_06_LinkedList/LinkedList_01.java
+++ Level-1/DSA_06_LinkedList/LinkedList_01.java
@@ -115,4 +115,160 @@ public class LinkedList_01 {
 
         return getNodeAt(idx).data;
     }
+
+    public Node removeLastNode(){
+        Node node = this.tail;
+        if(this.size == 0){
+            this.head = this.tail = null;
+        }else{
+            Node secondLast = getNodeAt(this.size - 2);
+            secondLast.next = null;
+            this.tail = secondLast;
+        }
+        this.size--;
+        return node;
+    }
+
+    public int removeLast(){
+        if(this.size == 0){
+            return -1;
+        }
+        return removeLastNode().data;
+    }
+
+    private void addNodeAt(Node node, int idx) {
+        if (idx == 0)
+            addFirstNode(node);
+        else if (idx == this.size)
+            addLastNode(node);
+        else {
+            Node prevNode = getNodeAt(idx - 1);
+            Node forwNode = prevNode.next;
+
+            prevNode.next = node;
+            node.next = forwNode;
+            this.size++;
+        }
+    }
+
+    public void addAt(int idx, int data) {
+        if (idx < 0 || idx > this.size)
+            return;
+
+        Node node = new Node(data);
+        addNodeAt(node, idx);
+    }
+
+    private Node removeNodeAt(int idx) {
+        if (idx == 0)
+            return removeFirstNode();
+        else if (idx == this.size - 1)
+            return removeLastNode();
+        else {
+            Node prevNode = getNodeAt(idx - 1);
+            Node node = prevNode.next;
+            Node forwNode = node.next;
+
+            node.next = null;
+            prevNode.next = forwNode;
+            this.size--;
+
+            return node;
+        }
+
+    }
+
+    public int removeAt(int idx) {
+        if (idx < 0 || idx >= this.size)
+            return -1;
+
+        return removeNodeAt(idx).data;
+    }
+
+    // LC 876
+    public Node middleNode(Node head) {
+        if (head == null || head.next == null)
+            return head;
+
+        Node slow = head, fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return slow;
+    }
+
+    // LC 19
+    public Node removeNthFromEnd(Node head, int n) {
+        if (head == null || head.next == null)
+            return null;
+
+        Node slow = head, fast = head;
+        while (n-- > 0)
+            fast = fast.next;
+
+        if (fast == null)
+            return head = head.next;
+
+        while (fast.next != null) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+
+        slow.next = slow.next.next;
+
+        return head;
+    }
+
+    public void oddEven() {
+        if (this.size == 0 || this.size == 1)
+            return;
+        Node even = new Node(-1); // Dummy Node
+        Node ep = even;
+
+        Node odd = new Node(-1); // Dummy Node
+        Node op = odd;
+
+        Node curr = this.head;
+        while (curr != null) {
+            if (curr.data % 2 == 0) {
+                ep.next = curr;
+                ep = ep.next;
+            } else {
+                op.next = curr;
+                op = op.next;
+            }
+
+            curr = curr.next;
+        }
+
+        op.next = even.next;
+        ep.next = null;
+
+        this.head = odd.next;
+        if (even.next != null)
+            this.tail = ep;
+        else
+            this.tail = op;
+    }
+
+    // 206
+    public static Node reverseList(Node head) {
+        if (head == null || head.next == null)
+            return head;
+
+        Node prev = null;
+        Node curr = head;
+        while (curr != null) {
+            Node forw = curr.next; // backup
+
+            curr.next = prev; // link
+
+            prev = curr; // move
+            curr = forw;
+        }
+
+        return prev;
+    }
 }
