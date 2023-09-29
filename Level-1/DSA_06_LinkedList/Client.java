0e0bdfdd1ef190bdeaf2eb5133aab1700c6a30dc
Mohak Singh <75531763+mohak3singh@users.noreply.github.com>
2023-09-29 07:16:32 +0000

    LinkedList added

---
+++ Level-1/DSA_06_LinkedList/Client.java
@@ -0,0 +1,8 @@
+package DSA_06_LinkedList;
+
+public class Client {
+    public static void main(String[] args) {
+        LinkedList_01 ll = new LinkedList_01();
+        ll.addFirst(10);
+    }
+}
